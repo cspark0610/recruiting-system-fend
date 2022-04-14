620235c24e5a129224513bbc67e6b4c02a84a7ca
Nacho Hotz <juanhotz611@gmail.com>
2022-04-14 12:41:14 -0300

    FEAT: candidate actions types

--- src/redux/candidates/types/index.ts
+++ src/redux/candidates/types/index.ts
@@ -1,14 +1,20 @@
-export const ADD_CANDIDATE = "ADD_CANDIDATE";
-export const ADD_CANDIDATE_SUCCESS = "ADD_CANDIDATE_SUCCESS";
-export const ADD_CANDIDATE_ERROR = "ADD_CANDIDATE_ERROR";
+export enum ActionTypes {
+  GET_CANDIDATES = 'GET_CANDIDATES',
+}
 
-export const GET_ID = "GET_ID";
+export const ADD_CANDIDATE = 'ADD_CANDIDATE';
+export const ADD_CANDIDATE_SUCCESS = 'ADD_CANDIDATE_SUCCESS';
+export const ADD_CANDIDATE_ERROR = 'ADD_CANDIDATE_ERROR';
 
-export const GET_DATA = "GET_DATA";
-export const GET_DATA_SUCCESS = "GET_DATA_SUCCESS";
-export const GET_DATA_ERROR = "GET_DATA_ERROR";
+export const GET_ID = 'GET_ID';
 
-export const GET_DATA_EDIT = "GET_DATA_EDIT";
-export const DATA_EDIT = "DATA_EDIT";
-export const DATA_EDIT_SUCCESS = "DATA_EDIT_SUCCESS";
-export const DATA_EDIT_ERROR = "DATA_EDIT_ERROR";
+export const GET_CANDIDATES = 'GET_CANDIDATES';
+
+export const GET_DATA = 'GET_DATA';
+export const GET_DATA_SUCCESS = 'GET_DATA_SUCCESS';
+export const GET_DATA_ERROR = 'GET_DATA_ERROR';
+
+export const GET_DATA_EDIT = 'GET_DATA_EDIT';
+export const DATA_EDIT = 'DATA_EDIT';
+export const DATA_EDIT_SUCCESS = 'DATA_EDIT_SUCCESS';
+export const DATA_EDIT_ERROR = 'DATA_EDIT_ERROR';
