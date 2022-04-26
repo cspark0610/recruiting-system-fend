95af549d650dbee32415b9fd586ada33e7684815
Nacho Hotz <juanhotz611@gmail.com>
2022-04-26 13:00:39 -0300

    CHECK: NODE_ENV for prod url

--- src/config/api/axios.ts
+++ src/config/api/axios.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 const { NODE_ENV } = process.env;
+console.log(NODE_ENV);
 
 const ClientAxios = axios.create({
   /* API where I will post and get information from my database */
