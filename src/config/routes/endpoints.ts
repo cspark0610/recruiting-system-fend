14bdc146e7d73afa2ec916e3d50fab504a27d2c8
Nacho Hotz <juanhotz611@gmail.com>
2022-04-26 10:37:29 -0300

    FEAT: endpoints based on NODE_ENV

--- src/config/routes/endpoints.ts
+++ src/config/routes/endpoints.ts
@@ -1,10 +1,19 @@
+const { NODE_ENV } = process.env;
+
 /* ENDPOINTS FROM LINK 1 */
 export const POST_USER = '/users';
 
 /* ENDPOINTS FROM LINK 2 */
 export const POST_CANDIDATE = '/candidates';
 export const CANDIDATE_BASE_URL =
-  'https://fulltimeforce-video-interview.herokuapp.com/candidate';
+  NODE_ENV === 'development'
+    ? 'http://localhost:3001/candidate'
+    : 'https://fulltimeforce-video-interview.herokuapp.com/candidate';
+
+export const JOB_BASE_URL =
+  NODE_ENV === 'development'
+    ? 'http://localhost:3001/job'
+    : 'https://fulltimeforce-video-interview.herokuapp.com/job';
 
 /* ENDPOINTS FROM ADMIN */
 export const POST_FEED = '/feeds';
