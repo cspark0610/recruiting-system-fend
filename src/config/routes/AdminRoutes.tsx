c74327f8343d09a77dbba06541016c0574785e76
Nacho Hotz <juanhotz611@gmail.com>
2022-04-26 11:15:26 -0300

    FEAT: navbar in all admin routes

--- src/config/routes/AdminRoutes.tsx
+++ src/config/routes/AdminRoutes.tsx
@@ -1,11 +1,15 @@
 import { Routes, Route } from 'react-router-dom';
 import { VIEW_KANBAN } from './paths';
 import CandidateStatus from '../../views/admin/dashboard/CandidateStatus/CandidateStatus';
+import Navbar from '../../components/kanban/Navbar';
 
 export default function AdminRoutes() {
   return (
-    <Routes>
-      <Route path={VIEW_KANBAN} element={<CandidateStatus />} />
-    </Routes>
+    <>
+      <Navbar userName="Juan" />
+      <Routes>
+        <Route path={VIEW_KANBAN} element={<CandidateStatus />} />
+      </Routes>
+    </>
   );
 }
