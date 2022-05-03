8c86a8286ae8f1d4973e8d4eda22044ecd33a02b
Nacho Hotz <juanhotz611@gmail.com>
2022-05-03 17:03:29 -0300

    FIX: axios config

--- src/config/api/axios.ts
+++ src/config/api/axios.ts
@@ -1,14 +1,13 @@
-import axios from "axios";
+import axios from 'axios';
 
 const { NODE_ENV } = process.env;
 
 const ClientAxios = axios.create({
   /* API where I will post and get information from my database */
-  /* baseURL:
+  baseURL:
     NODE_ENV === 'development'
       ? 'http://localhost:3001'
-      : 'https://fulltimeforce-video-interview.herokuapp.com', */
-  baseURL: "http://localhost:4000",
+      : 'https://fulltimeforce-video-interview.herokuapp.com',
 });
 
 export default ClientAxios;
