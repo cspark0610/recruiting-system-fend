aa7e64afdf3f2f76ddba650dcd112afc4ccb32be
Nacho Hotz <juanhotz611@gmail.com>
2022-05-04 17:22:43 -0300

    FEAT: LoaderSpinner component

--- src/components/openPositions/Item.tsx
+++ src/components/openPositions/Item.tsx
@@ -4,6 +4,7 @@ import { AiOutlineDown } from 'react-icons/ai';
 import { State } from '../../redux/store/store';
 import Toggle from './Toggle';
 import SubMenu from './SubMenu';
+import LoaderSpinner from '../../assets/loaderSpinner';
 
 type ItemProps = {
   positionName: string;
@@ -39,24 +40,7 @@ export default function Item({
             />
           ) : null}
           {loading && isToggled ? (
-            <svg
-              className="h-4 w-4 mt-3 animate-spin text-cyan-500"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-            >
-              <circle
-                className="opacity-25"
-                cx="12"
-                cy="12"
-                stroke="currentColor"
-              ></circle>
-              <path
-                className="opacity-75"
-                fill="currentColor"
-                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-              ></path>
-            </svg>
+            <LoaderSpinner width="4" height="4" classes="mt-3" />
           ) : null}
           <div
             className={
