130415fb1b1ab08749a1d96ba01b21ccdb21fd1b
Nacho Hotz <juanhotz611@gmail.com>
2022-05-04 17:27:56 -0300

    FIX: loader spinner in create position form

--- src/components/forms/FrmPosition.tsx
+++ src/components/forms/FrmPosition.tsx
@@ -6,6 +6,7 @@ import Submit from '../buttons/Submit';
 import priorities from '../../config/positions/constants';
 import { createPosition } from '../../redux/positions/actions/PositionsActions';
 import { State } from '../../redux/store/store';
+import LoaderSpinner from '../../assets/loaderSpinner';
 
 type OptionValues = {
   id: number;
@@ -182,24 +183,7 @@ export default function FrmPosition() {
         {loading ? (
           <div className="flex space-x-2 mt-4 items-center justify-center">
             <span className="font-semibold">Creating New Position...</span>
-            <svg
-              className="h-14 w-12 animate-spin text-cyan-500"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-            >
-              <circle
-                className="opacity-25"
-                cx="12"
-                cy="12"
-                stroke="currentColor"
-              ></circle>
-              <path
-                className="opacity-75"
-                fill="currentColor"
-                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-              ></path>
-            </svg>
+            <LoaderSpinner width="12" height="14" />
           </div>
         ) : null}
       </section>
