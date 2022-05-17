9841639a84b26e6c745152b56ed259c7ffa57635
Nacho Hotz <juanhotz611@gmail.com>
2022-05-17 17:34:17 -0300

    FIX: remove unused import

--- src/components/dialog/panels/TabContainers/General.tsx
+++ src/components/dialog/panels/TabContainers/General.tsx
@@ -1,6 +1,6 @@
 import { useSelector } from 'react-redux';
 import { BsFillTelephoneFill } from 'react-icons/bs';
-import { FaMapMarkerAlt, FaRegFileWord, FaRegFilePdf } from 'react-icons/fa';
+import { FaMapMarkerAlt, FaRegFilePdf } from 'react-icons/fa';
 import { HiMail } from 'react-icons/hi';
 import { State } from '../../../../redux/store/store';
 
@@ -8,14 +8,14 @@ const General = () => {
   const details = useSelector((state: State) => state.info.detail);
   const url_link_2 = details.url_link_2;
 
-  const birthYear = details.birth_date?.split("-")[0];
+  const birthYear = details.birth_date?.split('-')[0];
   const age = new Date().getFullYear() - birthYear;
 
   const handleCopy = (text: string) => {
-    if (text === "" || !text) return;
+    if (text === '' || !text) return;
 
     navigator.clipboard.writeText(text);
-    alert("Link copied");
+    alert('Link copied');
   };
 
   return (
@@ -29,18 +29,18 @@ const General = () => {
               {details.name}
             </h2>
             <p>
-              {details.academic_training ? details.academic_training : "N/A"}
+              {details.academic_training ? details.academic_training : 'N/A'}
             </p>
             <div className="my-[10px]">
               <p>
                 Date of birth: &nbsp;
                 <span className="font-medium">
-                  {details.birth_date ? details.birth_date : "N/A"}
+                  {details.birth_date ? details.birth_date : 'N/A'}
                 </span>
               </p>
               <p>
                 Age: &nbsp;
-                <span className="font-medium">{age ? age : "N/A"}</span>
+                <span className="font-medium">{age ? age : 'N/A'}</span>
               </p>
             </div>
             <p className="flex gap-[10px] items-center">
@@ -90,9 +90,9 @@ const General = () => {
                 <a
                   target="_blank"
                   rel="noopener noreferrer"
-                  href={details.linkedin ? details.linkedin : "#"}
+                  href={details.linkedin ? details.linkedin : '#'}
                 >
-                  {details.linkedin ? details.linkedin : "N/A"}
+                  {details.linkedin ? details.linkedin : 'N/A'}
                 </a>
               </span>
             </p>
@@ -102,9 +102,9 @@ const General = () => {
                 <a
                   target="_blank"
                   rel="noopener noreferrer"
-                  href={details.portfolio ? details.portfolio : "#"}
+                  href={details.portfolio ? details.portfolio : '#'}
                 >
-                  {details.portfolio ? details.portfolio : "N/A"}
+                  {details.portfolio ? details.portfolio : 'N/A'}
                 </a>
               </span>
             </p>
@@ -114,7 +114,7 @@ const General = () => {
                 onClick={() => handleCopy(url_link_2)}
                 className="text-cyan-color hover:cursor-pointer"
               >
-                {url_link_2 ? url_link_2 : "N/A"}
+                {url_link_2 ? url_link_2 : 'N/A'}
               </span>
             </p>
           </div>
@@ -122,9 +122,9 @@ const General = () => {
         {/* SECOND GRID */}
         <div
           className={`w-full my-[48px] font-raleway text-[12px] ${
-            details.main_status === "interested"
-              ? "text-[#B3B9C0]"
-              : "text-gray-color"
+            details.main_status === 'interested'
+              ? 'text-[#B3B9C0]'
+              : 'text-gray-color'
           }`}
         >
           <div className="my-[12px]">
@@ -132,7 +132,7 @@ const General = () => {
             <textarea
               name="description"
               id="description"
-              value={details.working_reason ? details.working_reason : "N/A"}
+              value={details.working_reason ? details.working_reason : 'N/A'}
               className="resize-none bg-light-color/100 border-light-color border focus:bg-white focus:outline-none focus:border-cyan-color rounded-[10px] max-w-full w-[350px] h-[121px] py-3 px-4 leading-tight font-raleway my-3"
               maxLength={280}
               readOnly
@@ -140,24 +140,24 @@ const General = () => {
           </div>
           <div className="my-5 flex flex-row">
             <p>
-              Salary Expectations:{" "}
+              Salary Expectations:{' '}
               <span className="font-bold">
                 {details.salary_expectations
                   ? details.salary_expectations
-                  : "N/A"}
-              </span>{" "}
+                  : 'N/A'}
+              </span>{' '}
             </p>
             <p className="ml-[45px]">
-              Available from:{" "}
+              Available from:{' '}
               <span className="font-bold">
-                {details.available_from ? details.available_from : "N/A"}
+                {details.available_from ? details.available_from : 'N/A'}
               </span>
             </p>
           </div>
           <hr className="text-light-color w-[349.44px]" />
           <div className="my-5">
             <p>
-              English Evaluation:{" "}
+              English Evaluation:{' '}
               <span className="bg-light-color w-[43px] h-[28px] rounded-[5px] px-3 py-1">
                 N/A
               </span>
