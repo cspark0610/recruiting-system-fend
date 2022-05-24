cef01c4e9ce1c44a19706c6c6936491b9d74ff4d
Nacho Hotz <juanhotz611@gmail.com>
2022-05-24 15:01:09 -0300

    FIX: typos and cursor pointer on toggle label

--- src/components/openPositions/Toggle.tsx
+++ src/components/openPositions/Toggle.tsx
@@ -32,7 +32,7 @@ export default function Toggle({
   };
 
   return (
-    <label className="relative w-[4rem] h-[1.80rem] rounded-full bg-[#F0F0F4] mt-2">
+    <label className="relative w-[4rem] h-[1.80rem] rounded-full bg-[#F0F0F4] mt-2 hover:cursor-pointer">
       <input
         type="checkbox"
         className="opacity-0 w-0 h-0 cursor-pointer"
@@ -43,16 +43,16 @@ export default function Toggle({
         <span
           className={
             isToggled && !updating
-              ? "before:absolute before:top-0.5 before:left-[4px] before:bottom-[4px] before:content-[''] before:w-[1.5rem] before:h-[1.5rem] before:bg-white cursor-pointer before:bg-[#00ADEF] before:transition ease-in-out duration-1100 before:transform before:translate-x-8 before:rounded-full cursor-pointer"
-              : "before:absolute before:top-0.5 before:left-[4px] before:bottom-[4px] before:content-[''] before:w-[1.5rem] before:h-[1.5rem] before:bg-white cursor-pointer before:bg-[#475564] before:transition ease-in-out duration-1100 before:rounded-full before:transform before:translate-x-0 cursor-pointer"
+              ? "before:absolute before:top-0.5 before:left-[4px] before:bottom-[4px] before:content-[''] before:w-[1.5rem] before:h-[1.5rem] before:bg-white cursor-pointer before:bg-[#00ADEF] before:transition ease-in-out duration-1100 before:transform before:translate-x-8 before:rounded-full"
+              : "before:absolute before:top-0.5 before:left-[4px] before:bottom-[4px] before:content-[''] before:w-[1.5rem] before:h-[1.5rem] before:bg-white cursor-pointer before:bg-[#475564] before:transition ease-in-out duration-1100 before:rounded-full before:transform before:translate-x-0"
           }
         />
       ) : (
         <span
           className={
             isToggled && !updating
-              ? "before:absolute before:top-0.5 before:left-[4px] before:bottom-[4px] before:content-[''] before:w-[1.5rem] before:h-[1.5rem] before:bg-white cursor-pointer before:bg-[#475564] before:transition ease-in-out duration-1100 before:transform before:translate-x-0 before:rounded-full cursor-pointer"
-              : "before:absolute before:top-0.5 before:left-[4px] before:bottom-[4px] before:content-[''] before:w-[1.5rem] before:h-[1.5rem] before:bg-white cursor-pointer before:bg-[#00ADEF] before:transition ease-in-out duration-1100 before:rounded-full before:transform before:translate-x-8 cursor-pointer"
+              ? "before:absolute before:top-0.5 before:left-[4px] before:bottom-[4px] before:content-[''] before:w-[1.5rem] before:h-[1.5rem] before:bg-white cursor-pointer before:bg-[#475564] before:transition ease-in-out duration-1100 before:transform before:translate-x-0 before:rounded-full"
+              : "before:absolute before:top-0.5 before:left-[4px] before:bottom-[4px] before:content-[''] before:w-[1.5rem] before:h-[1.5rem] before:bg-white cursor-pointer before:bg-[#00ADEF] before:transition ease-in-out duration-1100 before:rounded-full before:transform before:translate-x-8"
           }
         />
       )}
