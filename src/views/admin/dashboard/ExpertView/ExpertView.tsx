8674eba3ddce453a27fbe4018ed2fd2916f57ae1
Cristhian <jobs.cluna@gmail.com>
2022-05-25 16:08:33 -0500

    FEAT: change page title to Expert

--- src/views/admin/dashboard/ExpertView/ExpertView.tsx
+++ src/views/admin/dashboard/ExpertView/ExpertView.tsx
@@ -1,11 +1,25 @@
+import { useEffect } from "react"
 
 
 const ExpertView = () => {
+    useEffect(()=>{
+        window.document.title = 'WorkAt - Expert';
+    },[])
   return (
     <div className="flex flex-col">
       <span className="flex justify-center text-[#475564] text-2xl font-semibold mt-32">
         Expert View
       </span>
+
+      <div>
+          <h2>Candidate Records</h2>
+          <span>
+              Search by Skills
+          </span>
+          <div>
+
+          </div>
+      </div>
     </div>
   )
 }
