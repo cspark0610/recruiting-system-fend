358e58054d770c41c3ba9e5c90a18e5cb7717af3
Cristhian <jobs.cluna@gmail.com>
2022-05-27 11:02:55 -0500

    add button for down CV and Comparison

--- src/views/admin/dashboard/ExpertView/ExpertView.tsx
+++ src/views/admin/dashboard/ExpertView/ExpertView.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react"
 import FormView from "../../../../components/forms/FormView";
+import {RiDownloadFill as DonwloadIcon, RiArrowLeftRightFill as SwitchIcon} from 'react-icons/ri'
 
 
 
@@ -11,8 +12,20 @@ const ExpertView = () => {
         window.document.title = 'WorkAt - Expert';
     },[])
   return (
-    <div>
+    <div className="flex flex-col">
         <FormView toggleStatus={toggleStatus} setToggleStatus={setToggleStatus}/>
+
+        {/* Buttons Holder */}
+        <div className="flex my-6">
+            <div className=" flex items-center justify-center w-12 h-12 bg-[#475564] rounded-full cursor-pointer">
+                <DonwloadIcon className="text-3xl text-[#FFFFFF] "/>
+            </div>
+            <div className="ml-6 flex items-center justify-center w-12 h-12 bg-[#00ADEF] rounded-full cursor-pointer">
+                <SwitchIcon className="text-3xl text-[#FFFFFF] "/>
+            </div>
+        </div>
+
+        
     </div>
   )
 }
