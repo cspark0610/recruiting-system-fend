ee92f2b645237a7cfdb10cb3f20c062f15da669e
cspark0610 <cpark0610@gmail.com>
2022-09-13 16:27:18 -0300

    feat-front/auth0

--- src/components/buttons/GoogleButton.tsx
+++ src/components/buttons/GoogleButton.tsx
@@ -1,39 +1,34 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef } from "react";
 
 type GoogleButtonProps = {
-  CLIENT_ID: string;
-  text: 'signin_with' | 'signup_with' | 'continue_with' | undefined;
-  handleSuccess: (result: any) => void;
+	CLIENT_ID: string;
+	text: "signin_with" | "signup_with" | "continue_with" | undefined;
+	handleSuccess: (result: any) => void;
 };
 
-export default function GoogleButton({
-  CLIENT_ID,
-  text,
-  handleSuccess,
-}: GoogleButtonProps) {
-  const divRef = useRef<HTMLDivElement>(null);
+export default function GoogleButton({ CLIENT_ID, text, handleSuccess }: GoogleButtonProps) {
+	const divRef = useRef<HTMLDivElement>(null);
 
-  useEffect(() => {
-    if (typeof window === 'undefined' || !window.google || !divRef.current) {
-      return;
-    }
+	useEffect(() => {
+		if (typeof window === "undefined" || !window.google || !divRef.current) {
+			return;
+		}
 
-    try {
-      window.google.accounts.id.initialize({
-        client_id: CLIENT_ID,
-        callback: handleSuccess,
-      });
-      window.google.accounts.id.renderButton(divRef.current, {
-        type: 'standard',
-        theme: 'outline',
-        text,
-        size: 'large',
-      });
-    } catch (error) {
-      console.error(error);
-    }
+		try {
+			window.google.accounts.id.initialize({
+				client_id: CLIENT_ID,
+				callback: handleSuccess,
+			});
+			window.google.accounts.id.renderButton(divRef.current, {
+				type: "standard",
+				theme: "outline",
+				text,
+				size: "large",
+			});
+		} catch (error) {
+			console.error(error);
+		}
+	}, [CLIENT_ID, handleSuccess, text]);
 
-  }, [CLIENT_ID, handleSuccess, text]);
-
-  return <div ref={divRef} className="flex justify-center mt-4"></div>;
+	return <div ref={divRef} className="flex justify-center mt-4"></div>;
 }
